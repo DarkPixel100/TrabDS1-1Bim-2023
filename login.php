4ad4992993b4b0f1ac4445ea277dcbe9861fefb3
sofiamachado <sofia25.machado@gmail.com>
2023-03-28 21:35:02 -0300

    Co-authored-by: Diego Fontes de Avila <DarkPixel100@users.noreply.github.com>

--- login.php
+++ login.php
@@ -19,7 +19,9 @@
     <?php
     if (!empty($_GET['msg'])) {
         if ($_GET['msg'] == 'OK') {
-            echo "<main> Por favor, faça Login </main>";
+            echo "<main> <h2> Por favor, faça Login</h2> </main>";
+        } else if ($_GET['msg'] == 'ERROR') {
+            echo mysqli_error();
         }
     }
     ?>
@@ -28,16 +30,16 @@
     </header>
 
     <main>
-        <h2>Login</h2>
         <!-- form ou div com form dentro pro login -->
         <form class="infoBox" action="./login_session.php" method="post" enctype="multipart/form-data" autocomplete="off">
             <label for="email">E-mail:</label>
             <input type="text" id="email" name="email" placeholder="fulano@gmail.com" required>
 
             <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" placeholder="xxxxxx" required>
+            <input type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" id="password" name="password" placeholder="....." required>
 
             <input type="submit" name="submit" value="Login" required>
+            <a href="./register.php"> <button type="button"> Registrar </Button> </a>
         </form>
     </main>
 
