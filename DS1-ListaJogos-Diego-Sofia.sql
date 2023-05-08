e7230707859e3ad82ad4f78f2b1879d0861dd6f0
DarkPixel100 <diegofavila20@gmail.com>
2023-05-07 23:00:28 -0300

    Database exported

--- DS1-ListaJogos-Diego-Sofia.sql
+++ DS1-ListaJogos-Diego-Sofia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 03:50 AM
+-- Generation Time: May 08, 2023 at 04:00 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -37,6 +37,57 @@ CREATE TABLE `cartuchos` (
   `imgpath` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historicoderemocao`
+--
+
+CREATE TABLE `historicoderemocao` (
+  `deletionUserID` int(11) NOT NULL,
+  `gameUserID` int(11) NOT NULL,
+  `gameID` int(11) NOT NULL,
+  `titulo` text NOT NULL,
+  `sistema` text DEFAULT NULL,
+  `ano` year(4) NOT NULL,
+  `empresa` text NOT NULL,
+  `dataremocao` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sistemas`
+--
+
+CREATE TABLE `sistemas` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `fabricante` text NOT NULL,
+  `ano` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
+(1, 'admin', 'admin@mail.com', '$2y$10$05C4JMUNfv6Qzse3k6V0aeR7JlwhJ810VLyd78AoD2WPIvuY7P0Q2', 1);
+
 --
 -- Indexes for dumped tables
 --
@@ -50,6 +101,25 @@ ALTER TABLE `cartuchos`
   ADD KEY `sistema` (`sistema`);
 
 --
+-- Indexes for table `historicoderemocao`
+--
+ALTER TABLE `historicoderemocao`
+  ADD PRIMARY KEY (`gameID`);
+
+--
+-- Indexes for table `sistemas`
+--
+ALTER TABLE `sistemas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nome` (`nome`) USING HASH;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -58,6 +128,18 @@ ALTER TABLE `cartuchos`
 --
 ALTER TABLE `cartuchos`
   MODIFY `gameID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `sistemas`
+--
+ALTER TABLE `sistemas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
